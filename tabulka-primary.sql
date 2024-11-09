9f2434f0a090bc623772542f6820983e09ba5ad1
Eliška Kerhartová <eliska.kerhartova@seznam.cz>
2024-11-09 19:08:51 +0100

    Úprava tabulky primary

--- tabulka-primary.sql
+++ tabulka-primary.sql
@@ -1,23 +1,12 @@
-SELECT payroll_year 
-FROM czechia_payroll AS payroll;
-
-SELECT date_from 
-FROM czechia_price AS price;
-
-SELECT year 
-FROM economies AS eco;
-
 
 CREATE TABLE t_eliska_kerhartova_project_SQL_primary_final AS
-SELECT payroll.payroll_year
-	, payroll.industry_branch_code 
+SELECT payroll.payroll_year AS year
+	, payroll.industry_branch_code AS branch_code 
 	, branch.name AS branch_name
 	, payroll.value AS payroll_value
-	, YEAR(price.date_from) AS price_year
 	, price.category_code
 	, category.name AS category_name
-	, price.value AS price_value
-	, eco.year AS eco_year 
+	, price.value AS category_value
 	, eco.country
 	, eco.GDP
 FROM czechia_payroll AS payroll
@@ -29,8 +18,10 @@ JOIN economies AS eco
 	ON payroll.payroll_year = eco.YEAR
 	AND eco.country = 'Czech Republic'
 JOIN czechia_payroll_industry_branch AS branch
-	ON payroll.industry_branch_code = branch.code;
-
-
-SELECT *
-FROM t_eliska_kerhartova_project_SQL_primary_final;
+	ON payroll.industry_branch_code = branch.code
+JOIN czechia_payroll_value_type AS value_type
+	ON payroll.value_type_code = value_type.code
+	AND value_type.code = 5958
+JOIN czechia_payroll_unit AS payroll_unit
+	ON payroll.unit_code = payroll_unit.code
+	AND payroll_unit.code = 200;
