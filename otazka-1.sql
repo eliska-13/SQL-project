5ffe937a2cba8040f7777599a51175d621ec2c14
Eliška Kerhartová <eliska.kerhartova@seznam.cz>
2024-11-11 11:52:42 +0100

    Úprava SQL sady pro otázku 1

--- otazka-1.sql
+++ otazka-1.sql
@@ -11,8 +11,7 @@ SELECT branch_code
 	, MAX(year)  AS max_year
 FROM t_eliska_kerhartova_project_sql_primary_final AS tekpspf 
 GROUP BY branch_code, branch_name;
-
-
+ 
 
 CREATE VIEW ek_payroll_comparison AS
 SELECT
@@ -43,4 +42,30 @@ FROM ek_payroll_comparison
 WHERE current_year = 2018;
 
 
--- dlouho se načítá, zkusit jinak
+CREATE TEMPORARY TABLE ek_temporary_payroll_comparison AS
+SELECT
+	branch_code
+	, branch_name
+	, year 
+	, AVG(payroll_value) AS avg_payroll
+FROM t_eliska_kerhartova_project_sql_primary_final AS tekpspf
+GROUP BY branch_code, branch_name, year;
+
+
+SELECT
+	a.branch_code
+	, a.branch_name
+	, a.year AS current_year
+	, a.avg_payroll AS current_payroll_value
+	, b.year AS previous_year
+	, b.avg_payroll AS previous_payroll_value
+	, CASE
+		WHEN a.avg_payroll > b.avg_payroll THEN 'mzdy rostou'
+		WHEN a.avg_payroll = b.avg_payroll THEN 'mzdy stagnují'
+		WHEN a.avg_payroll < b.avg_payroll THEN 'mzdy klesají'
+	END AS payroll_trend
+FROM ek_temporary_payroll_comparison AS a
+JOIN ek_temporary_payroll_comparison AS b
+	ON a.branch_code = b.branch_code
+	AND a.year = b.year + 1;
+	
